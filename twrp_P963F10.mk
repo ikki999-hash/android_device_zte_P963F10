0eea02d129c5297e39d37328674f0853fcf2f2e3
ikki999-hash <57011046+ikki999-hash@users.noreply.github.com>
2026-05-26 10:29:35 +0300

    Refactor TWRP configuration for P963F10 device
    Updated device properties and inheritance for TWRP.

--- twrp_P963F10.mk
+++ twrp_P963F10.mk
@@ -5,24 +5,21 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
+# Inherit from standard 32-bit core product configuration layers
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_32_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit from TWRP product configurations (Sets up the recovery core libraries)
+$(call inherit-product, vendor/twrp/config/twrp.mk)
 
-# Inherit from P963F10 device
+# Inherit from your customized local device makefile configurations
 $(call inherit-product, device/zte/P963F10/device.mk)
 
+# Device Identification Properties Configuration
 PRODUCT_DEVICE := P963F10
-PRODUCT_NAME := omni_P963F10
-PRODUCT_BRAND := ZTE
+PRODUCT_NAME := twrp_P963F10
+PRODUCT_BRAND := zte
 PRODUCT_MODEL := ZTE Blade A35 Core
 PRODUCT_MANUFACTURER := zte
 
-PRODUCT_GMS_CLIENTID_BASE := android-zte
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="s9863a1h10_go_32b_2g-user 13 TP1A.220624.014 20240524.042327 release-keys"
-
-BUILD_FINGERPRINT := ZTE/P963F10_2/P963F10:13/TP1A.220624.014/20240524.042327:user/release-keys
+PRODUCT_GMS_VERSION_CHARACTERISTIC := oem_p963f10_go
